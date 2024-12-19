ed27a11b59655dfbe2a381a3270bdf9d0a2bc525
bitcoin-jim <bitcoinjimandfriends@pm.me>
2024-12-20 10:18:27 +1100

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Avuncular Group v0.0.1 (https://bitcoin-jim.github.io/avuncular-group)
+* Copyright 2013-2024 Avuncular Group Pty Ltd
+* Licensed under MIT (https://github.com/StartBootstrap/avuncular-group/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
